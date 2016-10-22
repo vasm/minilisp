8767afeb0c49f79be8f51bdb93f19e8e9148e5b5
Vasiliy Sabadazh <vsabadazh@gmail.com>
2016-10-22 15:49:35 +0300

    Initial Commit

---
+++ minilisp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  minilisp
+//
+//  Created by vas on 22.10.16.
+//  Copyright © 2016 vas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
